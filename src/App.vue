5f1aac6b9adbd9ae1df990b67015f189fc2f3154
kkikki486 <kkikki486@gmail.com>
2021-02-13 20:33:34 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>{{ animal }} </h1>
+  <h2>원숭이는 {{ food }}</h2>
+  <h3 v-text="food"></h3>
+  <input type="text" v-model="food">
+  <div v-html="alertMessage"></div>
+  
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      animal : "Monkey",
+      food : "apple",
+      alertMessage : "<h2>경고!!</h2>",
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+input { font-size: 20px; }
+</style>
